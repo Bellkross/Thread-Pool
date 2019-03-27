9e0d671cbabb5be9e56ebf73a9805fd3a62c9616
Kyrylo Vasylenko <vasilenkowm@gmail.com>
2019-03-27 23:27:38 +0200

    Create work provider implementation
    Signed-off-by: Kyrylo Vasylenko <vasilenkowm@gmail.com>

---
+++ src/main/java/work_provider/WorkProviderImpl.java
@@ -0,0 +1,43 @@
+package work_provider;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class WorkProviderImpl implements WorkProvider {
+
+    private final Queue<Runnable> jobQueue;
+    private int capacity;
+
+    public WorkProviderImpl(final int capacity) {
+        jobQueue = new LinkedList<>();
+        this.capacity = capacity;
+    }
+
+    public void addJob(final Runnable runnable) throws WorkQueueIsFullException {
+        if (jobQueue.size() >= capacity) {
+            throw new WorkQueueIsFullException();
+        }
+        synchronized (jobQueue) {
+            jobQueue.add(runnable);
+            jobQueue.notifyAll();
+        }
+    }
+
+    public Runnable getJob() {
+        synchronized (jobQueue) {
+            while (jobQueue.isEmpty()) {
+                try {
+                    jobQueue.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            return jobQueue.poll();
+        }
+    }
+
+    @Override
+    public void removeAllJobs() {
+        jobQueue.clear();
+    }
+}
