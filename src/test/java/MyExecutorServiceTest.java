ae1d02a3eeadb0eff7f52d8b06d7a6965bb0fce9
Kyrylo Vasylenko <vasilenkowm@gmail.com>
2019-03-28 00:36:09 +0200

    Add asyncStackPushingExecutionTest
    Signed-off-by: Kyrylo Vasylenko <vasilenkowm@gmail.com>

--- src/test/java/MyExecutorServiceTest.java
+++ src/test/java/MyExecutorServiceTest.java
@@ -3,6 +3,12 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import work_provider.WorkQueueIsFullException;
 
+import java.util.Stack;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 @RunWith(JUnit4.class)
 public class MyExecutorServiceTest {
 
@@ -19,7 +25,41 @@ public class MyExecutorServiceTest {
         }
     }
 
+    @Test
+    public void asyncStackPushingExecutionTest() throws InterruptedException {
+        int poolSize = 2;
+        int workQueueSize = 2;
+        MyExecutorService myExecutorService = MyExecutors.newFixedThreadPool(poolSize, workQueueSize);
 
+        Stack<Integer> stack = new Stack<>();
+        AtomicBoolean hasResult = new AtomicBoolean(false);
+        myExecutorService.execute(() -> {
+            try {
+                synchronized (stack) {
+                    stack.wait();
+                }
+                stack.push(1);
+                synchronized (hasResult) {
+                    hasResult.set(true);
+                    hasResult.notifyAll();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        myExecutorService.execute(() -> {
+            stack.push(2);
+            synchronized (stack) {
+                stack.notifyAll();
+            }
+        });
+        synchronized (hasResult) {
+            hasResult.wait();
+        }
+        assertTrue(hasResult.get());
+        assertEquals(1, stack.pop().intValue());
+        assertEquals(2, stack.pop().intValue());
+    }
 
 
 }
