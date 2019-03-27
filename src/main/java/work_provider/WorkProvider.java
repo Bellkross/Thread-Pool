fefe529112ebe4879f59bbd47fee10639d4eb2ce
Kyrylo Vasylenko <vasilenkowm@gmail.com>
2019-03-27 23:27:06 +0200

    Create WorkProvider interface
    Signed-off-by: Kyrylo Vasylenko <vasilenkowm@gmail.com>

---
+++ src/main/java/work_provider/WorkProvider.java
@@ -0,0 +1,9 @@
+package work_provider;
+
+public interface WorkProvider {
+
+    void addJob(Runnable runnable) throws WorkQueueIsFullException;
+    Runnable getJob();
+    void removeAllJobs();
+
+}
